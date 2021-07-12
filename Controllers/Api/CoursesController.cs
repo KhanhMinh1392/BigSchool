8b95293307a2c41bd158628d390683ebdae8fae1
Khanh Minh Dao <minhdao933@gmail.com>
2021-07-12 11:14:14 +0700

    commit 12/07

--- Controllers/Api/CoursesController.cs
+++ Controllers/Api/CoursesController.cs
@@ -22,7 +22,9 @@ namespace BigSchool.Controllers.Api
             var userId = User.Identity.GetUserId();
             var course = _dbContext.Courses.Single(c => c.Id == id && c.LecturerId == userId);
             if (course.IsCanceled)
+            {
                 return NotFound();
+            }   
             course.IsCanceled = true;
             _dbContext.SaveChanges();
             return Ok();
